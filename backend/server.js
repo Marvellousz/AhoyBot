8ec04e40fd50db38db4617766372ad91f23fe636
Marvellousz <pranavmurali024@gmail.com>
2025-05-01 16:19:56 +0530

    added process.env

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 dotenv.config();
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
